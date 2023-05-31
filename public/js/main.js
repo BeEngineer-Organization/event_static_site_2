5f1cfee66c919efe568d94cadff5682310119e2c
ユーザー名 <>
2023-05-31 20:35:58 +0900

    done

---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("firstview").addEventListener("click", () => {
+    document.getElementById("aiu").setAttribute("src", "https://i.ytimg.com/vi/J7pAsjgVbpQ/maxresdefault.jpg")
+})
