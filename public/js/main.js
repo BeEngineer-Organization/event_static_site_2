a81acc1bd7da1154e9e6cb86c39ff3b66e55e45e
1rishu1 <sayuri-no-mail@softbank.ne.jp>
2023-05-31 20:35:58 +0900

    done

---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("firstview").addEventListener("click", () => {
+    document.getElementById("firstview__image2").setAttribute("src", "https://i3.ytimg.com/vi/ZQveWRj0u5E/maxresdefault.jpg")
+})
