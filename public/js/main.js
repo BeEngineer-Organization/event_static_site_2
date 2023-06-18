11a94bc1b02d267be58fd316b7b6ed099babc45e
ユーザー名 <>
2023-06-18 12:01:35 +0900

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('header-button').addEventListener("click", (()=>{
+    // console.log(document.getElementsByClassName("firstview__title"))
+    document.getElementsByClassName("firstview__title")[0].classList.toggle('green-color') 
+}))
+
