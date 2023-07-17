bbd8ec5ece5684f79a5b8a5463ff216650376b48
HirotoInomoto <hiroto_inomoto@demia.co.jp>
2023-07-17 13:26:59 +0900

    taiken

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+const firstview = document.getElementById("firstview");
+firstview.addEventListener("click", () => {
+    document.getElementById("firstview__image").src = "https://www.conan-portal.com/img/chara01.jpg";
+});
