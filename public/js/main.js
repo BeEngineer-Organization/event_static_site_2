0a3bdfae21b71e2d1d3a5a31c2e864caf50a360b
HirotoInomoto <hiroto_inomoto@demia.co.jp>
2023-12-02 15:03:11 +0900

    体験会

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+document.querySelector(".header__image").addEventListener("click",()=>{
+    document.querySelector(".firstview__title").style.bottom = "99px"
+    document.querySelector(".firstview__title").style.opacity = "1"
+    document.querySelector(".firstview__title").style.color = "#ff58b7"
+    // document.querySelector(".firstview__title").classList.add("firstview__title__active")
+    
+})
