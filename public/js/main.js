0f353e858b4324282d07408f3ff0ab0eca3484e7
BeEngineer-DeMiA <beengineer.student@gmail.com>
2024-03-16 13:29:47 +0900

    20240317_9

---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+document.querySelector(".firstview__title").addEventListener("click",()=>{
+    document.querySelector(".firstview__image").src="https://www.konami.com/products_master_kam/jp_publish/am_qk_stadium/jp/ja/images/qks_top.jpg";
+})
