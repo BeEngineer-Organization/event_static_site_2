922a99f1afd8ba31aa8ebf9bc2a2555acf3adf4a
beengineer-demia <beengineer.student@gmail.com>
2024-03-16 13:32:40 +0900

    deploy

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+document.querySelector(".header__image").addEventListener("click",()=>{
+    document.querySelector(".firstview__image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO8pfTOW4ZbfXQNMDfa5bmaDNUjaL5uheIFnihwMVfbw&s"
+    document.querySelector(".firstview__title").style.backgroundColor="white"
+    document.querySelector(".firstview__title").style.color="black"
+});
