fadab311ae5aeab36ac76ce8c2b2b09ec9e2bc0d
HirotoInomoto <hiroto_inomoto@demia.co.jp>
2024-03-31 11:49:55 +0900

    deploy!

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("clickobj").addEventListener("click",()=>{
+    document.getElementById("title").style.opacity=1;
+    document.getElementById("title").style.left="100%";
+})
