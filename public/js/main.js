1ecde5124d84a7d9e98fb91c051b0523c12b4e8e
HirotoInomoto <hiroto_inomoto@demia.co.jp>
2024-07-15 12:08:04 +0900

    minami_sotaro

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+// const menu = document.getElementById("header__image")
+
+// menu.addEventListener("click",function(ev){
+//     document.getElementById("firstview__image").src = "./images/kanure.jpg";
+// }
+// );
