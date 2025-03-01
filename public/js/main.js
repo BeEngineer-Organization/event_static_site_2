e3214e24bdd4da11916e5fc424e2cda01774f95d
tobama <matoba>
2025-03-01 15:27:41 +0900

    体験会20250301

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+const botton = document.getElementById("botton")
+const image = document.getElementById("image-kimetu")
+botton.addEventListener("click",function(){
+    // botton.style.fontSize = "300px";
+    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Qwh6bIOh9flRxjxoz7gxbqkjLmWVUh6FoQ&s";
+})
